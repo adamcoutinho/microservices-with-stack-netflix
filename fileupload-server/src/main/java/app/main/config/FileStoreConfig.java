29b9b8889e214e965f53bfdcf270699357e6df05
adam <adamcoutinho@gmail.com>
2020-09-27 22:57:51 -0300

    adjuste

---
+++ fileupload-server/src/main/java/app/main/config/FileStoreConfig.java
@@ -0,0 +1,16 @@
+package app.main.config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "file")
+public class FileStoreConfig {
+
+    private String uploadDir;
+
+    public String getUploadDir() {
+        return uploadDir;
+    }
+
+    public void setUploadDir(String uploadDir) {
+        this.uploadDir = uploadDir;
+    }
+}
--- fileupload-server/src/main/java/com/main/fileuploadserver/FileuploadServerApplication.java
+++ fileupload-server/src/main/java/com/main/fileuploadserver/FileuploadServerApplication.java
@@ -1,13 +1,18 @@
 package com.main.fileuploadserver;
 
+import app.main.config.FileStoreConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
-@ComponentScan(value = {"app.main.endpoints"})
+@EnableConfigurationProperties({
+        FileStoreConfig.class
+})
+@ComponentScan(value = {"app.main.endpoints","app.main.services","app.main.config"})
 public class FileuploadServerApplication {
 
     public static void main(String[] args) {
